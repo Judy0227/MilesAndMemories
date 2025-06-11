2071e8e41f15e54655d3e95caca20f05bd324718
Ajiboye Adeoye <ajiboyeadeoye25@yahoo.com>
2025-06-11 15:02:10 +0100

    user login functionality added

--- src/domains/user/controller.js
+++ src/domains/user/controller.js
@@ -1,5 +1,37 @@
 const User = require("./model")
-const { hashData } = require("./../../utils/hashData");
+const { hashData, verifyHashedData } = require("./../../utils/hashData");
+const createToken = require("./../../utils/createToken");
+
+
+const authenticateUser = async (data) => {
+    try {
+        const { email, password } = data;
+
+        const fetchedUser = await User.findOne({email});
+
+        if (!fetchedUser) {
+            throw Error("Invalid credentials entered!");
+        }
+
+        const hashedPassword = fetchedUser.password;
+        const passwordMatch = await verifyHashedData(password, hashedPassword);
+
+        if (!passwordMatch) {
+            throw Error("Invalid password!");
+        }
+
+        // create user token
+        const tokenData = { userId: fetchedUser._id, email };
+        const token = await createToken(tokenData);
+
+        // assign user token
+        fetchedUser.token = token;
+        return fetchedUser;
+
+    } catch (error) {
+        throw error;
+    }
+}
 
 const createNewUser = async (data) => {
     try {
@@ -9,7 +41,7 @@ const createNewUser = async (data) => {
         const existingUser = await User.findOne({ email })
 
         if (existingUser) {
-            throw Error ("User with the provided email already exists")
+            throw Error ("User with the provided email already exists, please login")
         }
 
         // hash password
@@ -27,4 +59,4 @@ const createNewUser = async (data) => {
     }
 };
 
-module.exports = { createNewUser };
+module.exports = { createNewUser, authenticateUser };
--- src/utils/hashData.js
+++ src/utils/hashData.js
@@ -9,5 +9,13 @@ const hashData = async (data, saltRounds = 10) => {
     }
 };
 
+const verifyHashedData = async (unhashed, hashed) => {
+    try {
+        const match = await bcrypt.compare(unhashed, hashed);
+        return match;
+    } catch (error) {
+        throw error;
+    }
+}
 
-module.exports = { hashData };
+module.exports = { hashData, verifyHashedData };
