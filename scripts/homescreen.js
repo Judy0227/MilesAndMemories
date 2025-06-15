c437dfac57ced0e0c0c880cfc92957d9f8b9d49b
Judy0227 <136848121+Judy0227@users.noreply.github.com>
2025-06-15 20:13:27 +0100

    Delete scripts directory

--- scripts/homescreen.js
+++
@@ -1,5 +0,0 @@
-const firstName = localStorage.getItem('firstName') || 'Guest';
-const lastName = localStorage.getItem('lastName') || 'User';
-if (firstName && lastName) {
-  document.getElementById('profile-name').textContent = `${firstName} ${lastName}`;
-}
