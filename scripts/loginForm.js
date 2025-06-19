f0fd68106253f76895bcfb378ec153c124887344
Judy0227 <136848121+Judy0227@users.noreply.github.com>
2025-06-19 05:44:47 +0100

    Update loginForm.js

--- scripts/loginForm.js
+++ scripts/loginForm.js
@@ -41,7 +41,7 @@ password.addEventListener('blur', validatePassword);
 
     if (!re.test(password.value)) {
   const passwordError = document.getElementById('passwordError')
-        passwordError.textContent= 'Password should be at least 6 characters long and include a mix of uppercase and lowercase letters, numbers, and symbols';
+        passwordError.textContent= 'Password should be at least 12 characters long and include a mix of uppercase and lowercase letters, numbers, and symbols';
         password.classList.add('error')
     }  
     if (password.classList.contains('error')) {
@@ -57,4 +57,38 @@ import { handleLogin } from './auth.js';
 document.getElementById('loginForm').addEventListener('submit', (e) => {
   e.preventDefault();
   handleLogin();
+});
+// Import the functions you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  import { getAuth, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAsAaV_uiplyie0Ube0tZHJyzBZ-7fkR70",
+    authDomain: "milesandmemories-194c5.firebaseapp.com",
+    projectId: "milesandmemories-194c5",
+    storageBucket: "milesandmemories-194c5.appspot.com", // 🔧 Fixed typo: should be .app**spot**.com
+    messagingSenderId: "463886821501",
+    appId: "1:463886821501:web:3e679e228d0a6b97522914",
+    measurementId: "G-ZMMQ1BQ48Y"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+const forgotPasswordLink = document.getElementById("forgotPasswordLink");
+
+forgotPasswordLink.addEventListener("click", () => {
+  const email = prompt("Enter your email to reset password:");
+
+  if (email) {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        showMessage("Reset link sent! Check your email.", 'success') ;
+      })
+      .catch((error) => {
+        console.error("Password reset error:", error);
+        showMessage(`Error ${error.message}`, error);
+
+      });
+  }
 });
